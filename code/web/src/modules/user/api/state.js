15718d4509f6eb8b3d2044d40239a3ad2504a80c
Charles Wefso <cwefso@gmail.com>
2020-08-22 13:53:04 -0600

    Annotated user/api/state.js

--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -12,8 +12,13 @@ export const userInitialState = {
 }
 
 // State
+//state is defined as the above object. The action object is also passed in.
 export default (state = userInitialState, action) => {
+//The switch takes in the action type to determine the case.
   switch (action.type) {
+// This sets the initial state and then if 
+// the action.user object isn't empty it sets isAuthenticated property to true.
+// It sets the details to the value of the actions user property.
     case SET_USER:
       return {
         ...state,
@@ -21,6 +26,8 @@ export default (state = userInitialState, action) => {
         details: action.user,
       }
 
+//This sets the initial state, and then sets the error to null in case there was an error already in state.
+//It also sets the isLoading property to true. This begins the login process.
     case LOGIN_REQUEST:
       return {
         ...state,
@@ -28,6 +35,8 @@ export default (state = userInitialState, action) => {
         isLoading: action.isLoading
       }
 
+//This sets the initial state, and then sets the error as whatever the action object has as an error property
+//It also sets the isLoading property to false. This resolves the login.
     case LOGIN_RESPONSE:
       return {
         ...state,
@@ -35,6 +44,7 @@ export default (state = userInitialState, action) => {
         isLoading: false
       }
 
+//When the type of the action is LOGOUT, all four properties are set to null or false
     case LOGOUT:
       return {
         ...state,
@@ -44,6 +54,7 @@ export default (state = userInitialState, action) => {
         details: null
       }
 
+//The default that is returned is the userInitialState object value
     default:
       return state
   }
