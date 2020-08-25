bd4505c72f45637acaecca692404624b0229b9b8
Bill Wilke <billwilke2@gmail.com>
2020-08-24 20:23:26 -0600

    Reinstalled jest-dom, adds initial tests to Profile.js

--- code/web/src/modules/user/Profile.test.js
+++ code/web/src/modules/user/Profile.test.js
@@ -2,4 +2,66 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import { BrowserRouter, MemoryRouter } from 'react-router-dom';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import Profile from './Profile'
+
+
+describe('Profile', () => {
+  const store = createStore(() => ({
+    user: {
+      details: {
+        email: 'billwilke2@gmail.com',
+        name: 'bill wilke',
+        role: null
+      },
+      error: null,
+      isAuthenticated: true,
+      isLoading: false
+    },
+    logout: jest.fn(),
+    match: {},
+    staticContext: undefined,
+  }))
+
+  it('renders without crashing', () => {
+
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Profile />
+        </Provider>
+      </MemoryRouter> , div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('Should render a name', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Profile />
+        </Provider>
+      </BrowserRouter>
+
+    )
+    const name = getByText('bill wilke')
+    
+    expect(name).toBeInTheDocument()
+  })
+
+  it('Should render an email', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Profile />
+        </Provider>
+      </BrowserRouter>
+
+    )
+    const email = getByText('Email: billwilke2@gmail.com')
+    
+    expect(email).toBeInTheDocument()
+  })
+})
